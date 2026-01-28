b27ed5dc4f3732b9771c1f4720a9b36f73a4cd89
Alessio Illari <alessio.illari@uconn.edu>
2026-01-28 15:11:21 -0500

    Added peak approximating feature

--- lib/fitting/PeakFitter.cpp
+++ lib/fitting/PeakFitter.cpp
@@ -4,8 +4,71 @@
 #include <TH1.h>
 #include <TF1.h>
 
+//Setting global limits of fit sigma
+constexpr double MIN_SIGMA_FRACTION = 0.3;
+constexpr double MAX_SIGMA_FRACTION = 3.0;
+
 PeakFitter::PeakFitter(const FitConfig& cfg)
 : cfg_(cfg) {}
+
+PeakFitter::PeakGuess PeakFitter::guessPeak(TH1* h) const
+{
+    PeakGuess g;
+
+    // fallback defaults
+    g.mean  = h->GetMean();
+    g.sigma = cfg_.expected_sigma;
+    g.amplitude = h->GetMaximum();
+
+    std::unique_ptr<TH1> h_smooth(static_cast<TH1*>(h->Clone("h_smooth")));
+    h_smooth->Smooth(1);
+
+    TSpectrum spec(3);
+    int n_peaks = spec.Search(h_smooth.get(), 2, "", 0.05);
+
+    if (n_peaks <= 0)
+        return g;
+
+    double* peaks = spec.GetPositionX();
+
+    int best_bin = -1;
+    double best_height = -1;
+
+    for (int i = 0; i < n_peaks; ++i) {
+        int bin = h->FindBin(peaks[i]);
+        double height = h->GetBinContent(bin);
+        if (height > best_height) {
+            best_height = height;
+            best_bin = bin;
+        }
+    }
+
+    if (best_bin < 0)
+        return g;
+
+    g.mean = h->GetBinCenter(best_bin);
+    g.amplitude = best_height;
+
+    // local RMS estimate
+    int lo = h->FindBin(g.mean - 2 * cfg_.expected_sigma);
+    int hi = h->FindBin(g.mean + 2 * cfg_.expected_sigma);
+
+    double sum = 0, sumw = 0;
+    for (int i = lo; i <= hi; ++i) {
+        double x = h->GetBinCenter(i);
+        double w = h->GetBinContent(i);
+        sum += w * (x - g.mean) * (x - g.mean);
+        sumw += w;
+    }
+
+    if (sumw > 0)
+        g.sigma = std::sqrt(sum / sumw);
+
+    g.sigma = std::clamp(g.sigma, 0.5 * cfg_.expected_sigma, 2.0 * cfg_.expected_sigma);
+
+    return g;
+}
+
 
 FitResult PeakFitter::fit(TH1* h) const
 {
@@ -27,6 +90,8 @@ FitResult PeakFitter::fit(TH1* h) const
             best = r;
         }
     }
+
+    
 
     return best;
 }
